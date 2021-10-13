5e0a82423637223adb0422ca4c863936a3efd1a9
Synoecium <Synoecium@gmail.com>
2021-10-13 21:26:52 +0500

    checking input parameters improved

--- task2/task2/main.cpp
+++ task2/task2/main.cpp
@@ -12,32 +12,34 @@ using namespace std;
 int cmp2zcharCount(const char* str1,const char* str2, int startWithCount = -1)
 {
     int idx = 0;
-    while (*str1!='\0' && *str1==*str2 && (startWithCount<=0 || idx<startWithCount-1))
+    while (str1[idx]!='\0' && str1[idx]==str2[idx] && (startWithCount<=0 || idx<startWithCount-1))
     {
-        ++str1;
-        ++str2;
         ++idx;
     }
-    if (*str1==*str2) return 0;
-    if (startWithCount>=0 && *str1=='\0') return 0;
-    if (*str1>*str2) return 1;      //assuming '\0' less than any symbol
+    if (str1[idx]==str2[idx])
+        return 0;
+    if (startWithCount>=0 && str1[idx]=='\0')
+        return 0;
+    if (str1[idx]>str2[idx])
+        return 1;      //assuming '\0' less than any symbol
     return -1;
 }
 
 int substrFind(const char* str,const char* substr,int startWithCount = 0)
 {
-    if (str[0]=='\0')
-    {
-        if (substr[0]=='\0') return 1;
+    bool isStrEmpty = (str==nullptr || str[0]=='\0');
+    bool isSubstrEmpty = (substr==nullptr || substr[0]=='\0');
+    if (isStrEmpty && isSubstrEmpty)
+        return 1;
+    if (isStrEmpty || isSubstrEmpty)
         return 0;
-    }
-    if (substr[0]=='\0') return 0;
 
     unsigned k = 0;
 
     while (*str!='\0')
     {
-        if (cmp2zcharCount(substr,str,startWithCount)==0) k++;
+        if (cmp2zcharCount(substr,str,startWithCount)==0)
+            k++;
         ++str;
     }
 
@@ -52,7 +54,9 @@ int main()
     TEST("test greater",cmp2zcharCount("test","tes"),1);
     TEST("test equality 2 first chars",cmp2zcharCount("tegt2","test2", 2),0);
 
-
+    TEST("test nullptr in nullptr",substrFind(nullptr,nullptr),1);
+    TEST("test nullptr in string",substrFind(nullptr,"test"),0);
+    TEST("test string in nullptr",substrFind("test",nullptr),0);
     TEST("test empty in empty",substrFind("",""),1);
     TEST("test empty in string",substrFind("","test"),0);
     TEST("test string in empty",substrFind("test",""),0);
