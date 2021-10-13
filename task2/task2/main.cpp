a3c681ca9c05af994a4155a05ec1a5ceb8f04acd
Synoecium <Synoecium@gmail.com>
2021-10-13 15:43:02 +0500

    cmp2zcharCount function

--- task2/task2/main.cpp
+++ task2/task2/main.cpp
@@ -18,7 +18,26 @@ int cmp2zchar(const char* str1,const char* str2, bool isStartWith=false)
     return -1;
 }
 
-int substrFind(const char* str,const char* substr)
+// startWithCount value:
+//      -1 - strict equality
+//      0 - str2 start with str1
+//      >0 - str2 start with startWithCount first chars from str2
+int cmp2zcharCount(const char* str1,const char* str2, int startWithCount = -1)
+{
+    int idx = 0;
+    while (*str1!='\0' && *str1==*str2 && (startWithCount<=0 || idx<startWithCount-1))
+    {
+        ++str1;
+        ++str2;
+        ++idx;
+    }
+    if (*str1==*str2) return 0;
+    if (startWithCount>=0 && *str1=='\0') return 0;
+    if (*str1>*str2) return 1;      //assuming '\0' less than any symbol
+    return -1;
+}
+
+int substrFind(const char* str,const char* substr,int startWithCount = 0)
 {
     if (str[0]=='\0')
     {
@@ -31,7 +50,7 @@ int substrFind(const char* str,const char* substr)
 
     while (*str!='\0')
     {
-        if (cmp2zchar(substr,str,true)==0) k++;
+        if (cmp2zcharCount(substr,str,startWithCount)==0) k++;
         ++str;
     }
 
@@ -40,11 +59,12 @@ int substrFind(const char* str,const char* substr)
 
 int main()
 {
-    /*
-    TEST("test equality",cmp2zchar("test2","test2"),0);
-    TEST("test lesser",cmp2zchar("test1","test2"),-1);
-    TEST("test greater",cmp2zchar("test","tes"),1);
-    */
+
+    TEST("test equality",cmp2zcharCount("test2","test2"),0);
+    TEST("test lesser",cmp2zcharCount("test1","test2"),-1);
+    TEST("test greater",cmp2zcharCount("test","tes"),1);
+    TEST("test equality 2 first chars",cmp2zcharCount("tegt2","test2", 2),0);
+
 
     TEST("test empty in empty",substrFind("",""),1);
     TEST("test empty in string",substrFind("","test"),0);
@@ -54,6 +74,8 @@ int main()
     TEST("test 3 times with partial",substrFind("tetestdstesttetest","test"),3);
     TEST("test partial",substrFind("testestest","test"),3);
     TEST("test intersecting occurrences",substrFind("sssss","ss"),4);
+    TEST("test first 2 chars",substrFind("Testqwerrtytest","Test",2),1);
+    TEST("test first 2 chars harder",substrFind("TebtqwerrTytest","Test",2),1);
 
 
     return 0;
